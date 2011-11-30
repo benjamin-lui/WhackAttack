16489077ebccc5e91d3bfaabab5ed1e92585d53c
Benjamin Lui <benlui@berkeley.edu>
2011-11-29 21:31:15 -0800

    Initial Commit

---
+++ WhackAttack/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  WhackAttack
+//
+//  Created by Benjamin Lui on 11/29/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ WhackAttack/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  WhackAttack
+//
+//  Created by Benjamin Lui on 11/29/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
